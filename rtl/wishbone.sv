466fd928e427323dfe1c60d93ed4e7c1a8d28462
piledriven1 <sto08@calpoly.edu>
2025-05-23 11:35:30 -0700

    Confirm working wishbone. Verify working tests and itests on tb_wishbone

--- rtl/wishbone.sv
+++ rtl/wishbone.sv
@@ -4,43 +4,45 @@ module wishbone #(
     parameter A_WIDTH = 8
     ) (
     // common
-    input logic clk, rst,
+    input wire clk, rst,
 
     // port A
-    input logic pA_wb_stb_i,
-    input logic [A_WIDTH:0] pA_wb_addr_i,
-    input logic [3:0] pA_wb_we_i,
-    input logic [31:0] pA_wb_data_i,
-    output logic pA_wb_ack_o, pA_wb_stall_o,
-    output logic [31:0] pA_wb_data_o,
+    input wire pA_wb_stb_i,
+    input wire [A_WIDTH:0] pA_wb_addr_i,
+    input wire [3:0] pA_wb_we_i,
+    input wire [31:0] pA_wb_data_i,
+    output reg pA_wb_ack_o, pA_wb_stall_o,
+    output reg [31:0] pA_wb_data_o,
 
     // port B
-    input logic pB_wb_stb_i,
-    input logic [A_WIDTH:0]  pB_wb_addr_i,
-    input logic [3:0] pB_wb_we_i,
-    input logic [31:0] pB_wb_data_i,
-    output logic pB_wb_ack_o, pB_wb_stall_o,
-    output logic [31:0] pB_wb_data_o
+    input wire pB_wb_stb_i,
+    input wire [A_WIDTH:0]  pB_wb_addr_i,
+    input wire [3:0] pB_wb_we_i,
+    input wire [31:0] pB_wb_data_i,
+    output reg pB_wb_ack_o, pB_wb_stall_o,
+    output reg [31:0] pB_wb_data_o
 );
     logic turn;
     logic [3:0] ram0_wb_we0_i, ram1_wb_we0_i;
     logic ram0_wb_en0_i, ram1_wb_en0_i;
-    logic [A_WIDTH - 1: 0] ram0_wb_a0_i, ram1_wb_a0_i;
+    logic [A_WIDTH - 1: 0] ram0_wb_a0_i, ram1_wb_a0_i, ram_addrA, ram_addrB;
     logic [31:0] ram0_wb_Di0_i, ram1_wb_Di0_i;
     logic [31:0] ram0_wb_Do0_o, ram1_wb_Do0_o;
     logic ram_selA, ram_selB, conflict;
 
     assign ram_selA = pA_wb_addr_i[A_WIDTH];
     assign ram_selB = pB_wb_addr_i[A_WIDTH];
+    assign ram_addrA = pA_wb_addr_i[A_WIDTH - 1:0];
+    assign ram_addrB = pB_wb_addr_i[A_WIDTH - 1:0];
 
     assign conflict = pA_wb_stb_i && pB_wb_stb_i && (ram_selA == ram_selB);
 
     always_comb begin
         // Default values
-        pA_wb_ack_o = 0;
-        pB_wb_ack_o = 0;
         pA_wb_stall_o = 0;
         pB_wb_stall_o = 0;
+        pA_wb_data_o = 0;
+        pB_wb_data_o = 0;
         ram0_wb_en0_i = 0;
         ram1_wb_en0_i = 0;
         ram0_wb_we0_i = 0;
@@ -51,18 +53,7 @@ module wishbone #(
         ram1_wb_Di0_i = 0;
 
         if(rst) begin
-            pA_wb_ack_o = 0;
-            pB_wb_ack_o = 0;
-            pA_wb_stall_o = 0;
-            pB_wb_stall_o = 0;
-            ram0_wb_en0_i = 0;
-            ram1_wb_en0_i = 0;
-            ram0_wb_we0_i = 0;
-            ram1_wb_we0_i = 0;
-            ram0_wb_a0_i = 0;
-            ram1_wb_a0_i = 0;
-            ram0_wb_Di0_i = 0;
-            ram1_wb_Di0_i = 0;
+            
         end
         else begin
             if(conflict) begin
@@ -76,37 +67,40 @@ module wishbone #(
                     pB_wb_stall_o = 1;
                 end
             end
-        
+
             // port A RAM selection
             if(pA_wb_stb_i && !pA_wb_stall_o) begin
-                pA_wb_ack_o = 1;
                 if(ram_selA) begin
                     ram1_wb_en0_i = 1;
                     ram1_wb_we0_i = pA_wb_we_i;
-                    ram1_wb_a0_i = pA_wb_addr_i[A_WIDTH-1:0];
+                    ram1_wb_a0_i = ram_addrA;
                     ram1_wb_Di0_i = pA_wb_data_i;
+                    pA_wb_data_o = ram1_wb_Do0_o;
                 end
                 else begin
                     ram0_wb_en0_i = 1;
                     ram0_wb_we0_i = pA_wb_we_i;
-                    ram0_wb_a0_i = pA_wb_addr_i[A_WIDTH-1:0];
+                    ram0_wb_a0_i = ram_addrA;
                     ram0_wb_Di0_i = pA_wb_data_i;
+                    pA_wb_data_o = ram0_wb_Do0_o;
                 end
             end
+
             // port B RAM selection
             if(pB_wb_stb_i && !pB_wb_stall_o) begin
-                pB_wb_ack_o = 1;
                 if(ram_selB) begin
                     ram1_wb_en0_i = 1;
                     ram1_wb_we0_i = pB_wb_we_i;
-                    ram1_wb_a0_i = pB_wb_addr_i[A_WIDTH-1:0];
+                    ram1_wb_a0_i = ram_addrB;
                     ram1_wb_Di0_i = pB_wb_data_i;
+                    pB_wb_data_o = ram1_wb_Do0_o;
                 end
                 else begin
                     ram0_wb_en0_i = 1;
                     ram0_wb_we0_i = pB_wb_we_i;
-                    ram0_wb_a0_i = pB_wb_addr_i[A_WIDTH-1:0];
+                    ram0_wb_a0_i = ram_addrB;
                     ram0_wb_Di0_i = pB_wb_data_i;
+                    pB_wb_data_o = ram0_wb_Do0_o;
                 end
             end
         end
@@ -116,36 +110,43 @@ module wishbone #(
     always_ff @(posedge clk) begin
         if(rst) begin
             turn <= 0;
-            pA_wb_data_o <= 0;
-            pB_wb_data_o <= 0;
+            pA_wb_ack_o <= 0;
+            pB_wb_ack_o <= 0;
         end
         else begin
-            // if pA || pB
-            pA_wb_data_o <= (pA_wb_addr_i[A_WIDTH]) ? ram1_wb_Do0_o : ram0_wb_Do0_o;
-            pB_wb_data_o <= (pB_wb_addr_i[A_WIDTH]) ? ram1_wb_Do0_o : ram0_wb_Do0_o;
-
-            if(conflict && ((turn && pB_wb_stb_i) || (!turn && pA_wb_stb_i))) begin
+            if(conflict)
                 turn <= ~turn;
-            end
             else
                 turn <= turn;
+            
+            if(pA_wb_stb_i && !pA_wb_stall_o)
+                pA_wb_ack_o <= 1;
+            else
+                pA_wb_ack_o <= 0;
+
+            if(pB_wb_stb_i && !pB_wb_stall_o)
+                pB_wb_ack_o <= 1;
+            else
+                pB_wb_ack_o <= 0;
         end
     end
 
     // instantiate the RAM modules
-    DFFRAM256x32 RAM0(.CLK(clk),
-                                .EN0(ram0_wb_en0_i),
-                                .WE0(ram0_wb_we0_i),
-                                .A0(ram0_wb_a0_i[A_WIDTH - 1:0]),
-                                .Di0(ram0_wb_Di0_i),
-                                .Do0(ram0_wb_Do0_o)
-                                );
+    DFFRAM256x32 RAM0(
+        .CLK(clk),
+        .EN0(ram0_wb_en0_i),
+        .WE0(ram0_wb_we0_i),
+        .A0(ram0_wb_a0_i),
+        .Di0(ram0_wb_Di0_i),
+        .Do0(ram0_wb_Do0_o)
+    );
 
-    DFFRAM256x32 RAM1(.CLK(clk),
-                                .EN0(ram1_wb_en0_i),
-                                .WE0(ram1_wb_we0_i),
-                                .A0(ram1_wb_a0_i[A_WIDTH - 1:0]),
-                                .Di0(ram1_wb_Di0_i),
-                                .Do0(ram1_wb_Do0_o)
-                                );
+    DFFRAM256x32 RAM1(
+        .CLK(clk),
+        .EN0(ram1_wb_en0_i),
+        .WE0(ram1_wb_we0_i),
+        .A0(ram1_wb_a0_i),
+        .Di0(ram1_wb_Di0_i),
+        .Do0(ram1_wb_Do0_o)
+    );
 endmodule
--- tests/tb_wishbone/tb_wishbone.sv
+++ tests/tb_wishbone/tb_wishbone.sv
@@ -2,7 +2,7 @@
 
 module tb_wishbone();
     localparam A_WIDTH = 8;
-    localparam CLK_PERIOD = 45;
+    localparam CLK_PERIOD = 40;
 
     logic clk, rst;
     // port A
@@ -10,7 +10,7 @@ module tb_wishbone();
     logic [A_WIDTH:0] pA_wb_addr_i;
     logic [3:0] pA_wb_we_i;
     logic [31:0] pA_wb_data_i;
-    logic pA_wb_err_o, pA_wb_ack_o, pA_wb_stall_o;
+    logic pA_wb_ack_o, pA_wb_stall_o;
     logic [31:0] pA_wb_data_o;
 
     `ifdef USE_POWER_PINS
@@ -25,7 +25,7 @@ module tb_wishbone();
     logic [A_WIDTH:0] pB_wb_addr_i;
     logic [3:0] pB_wb_we_i;
     logic [31:0] pB_wb_data_i;
-    logic pB_wb_err_o, pB_wb_ack_o, pB_wb_stall_o;
+    logic pB_wb_ack_o, pB_wb_stall_o;
     logic [31:0] pB_wb_data_o;
 
     wishbone UUT(.*);
@@ -37,7 +37,19 @@ module tb_wishbone();
         $dumpvars(2, tb_wishbone);
 
         clk = 0;
+        rst = 0;
+        pA_wb_stb_i = 0;
+        pB_wb_stb_i = 0;
+        pA_wb_data_i = 0;
+        pB_wb_data_i = 0;
+        pA_wb_we_i = 0;
+        pB_wb_we_i = 0;
+
+        @(posedge clk);
         rst = 1;
+
+        @(posedge clk);
+        rst = 0;
         pA_wb_we_i = 15;
         pA_wb_addr_i = 9'b000000000;
         pA_wb_stb_i = 0;
@@ -48,7 +60,7 @@ module tb_wishbone();
         pB_wb_stb_i = 0;
         pB_wb_data_i = 0;
 
-        #CLK_PERIOD;
+        #(CLK_PERIOD * 2);
         rst = 0;
         pA_wb_stb_i = 1;
         pB_wb_stb_i = 1;
@@ -56,23 +68,116 @@ module tb_wishbone();
         @(posedge clk);
         pA_wb_data_i = 32'hdeaddead;
         pB_wb_data_i = 32'hfeedbeef;
-        @(posedge clk);
+
+        #(CLK_PERIOD * 3);
+        assert (pA_wb_data_o == 32'hdeaddead)
+        else begin
+            $error("pA_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hdeaddead);
+            $finish();
+        end
+        assert (pB_wb_data_o == 32'hfeedbeef)
+        else begin
+            $error("pB_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hfeedbeef);
+            $finish();
+        end
         pA_wb_we_i = 12;
         pB_wb_we_i = 3;
-        #CLK_PERIOD;
+
+        @(posedge clk);
         // Have both A & B access RAM 1
         pA_wb_addr_i = 9'b100000011;
         pB_wb_addr_i = 9'b100000011;
-        #CLK_PERIOD;
+
         @(posedge clk);
+        assert (pA_wb_stall_o == 1)
+        else begin
+            $error("pA_wB_stall_o Failed: 0x%x != 0x%x", pA_wb_stall_o, 1);
+            $finish();
+        end
+        assert (pB_wb_stall_o == 0)
+        else begin
+            $error("pB_wB_stall_o Failed: 0x%x != 0x%x", pB_wb_stall_o, 0);
+            $finish();
+        end
+
+        @(posedge clk);
+        assert (pA_wb_stall_o == 0)
+        else begin
+            $error("pA_wB_stall_o Failed: 0x%x != 0x%x", pA_wb_stall_o, 0);
+            $finish();
+        end
+        assert (pB_wb_stall_o == 1)
+        else begin
+            $error("pB_wB_stall_o Failed: 0x%x != 0x%x", pB_wb_stall_o, 1);
+            $finish();
+        end
+
         #(CLK_PERIOD * 2);
+        assert (pA_wb_data_o == 32'hdeadbeef)
+        else begin
+            $error("pA_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hdeadbeef);
+            $finish();
+        end
+
         // Have both A & B access different RAM modules
         pA_wb_data_i = 32'hfeedfeed;
         pA_wb_we_i = 15;
         pA_wb_addr_i = 9'b000000000;
         pB_wb_addr_i = 9'b100000011;
+
+        #(CLK_PERIOD * 2);
+        assert (pA_wb_data_o == 32'hdeaddead)
+        else begin
+            $error("pA_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hdeaddead);
+            $finish();
+        end
+
+        #CLK_PERIOD;
+        assert (pA_wb_data_o == 32'hfeedfeed)
+        else begin
+            $error("pA_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hfeedfeed);
+            $finish();
+        end
+
+        assert (pB_wb_data_o == 32'hfeedbeef)
+        else begin
+            $error("pB_wB_data_o Failed: 0x%x != 0x%x", pB_wb_data_o, 32'hfeedbeef);
+            $finish();
+        end
+
+        // Set A & B to RAM
+        pA_wb_addr_i = 9'b000110000;
+        pB_wb_addr_i = 9'b000110000;
+
         @(posedge clk);
-        #(CLK_PERIOD * 4);
+
+        #(CLK_PERIOD * 2);
+        assert (pA_wb_stall_o == 0)
+        else begin
+            $error("pA_wB_stall_o Failed: 0x%x != 0x%x", pA_wb_stall_o, 0);
+            $finish();
+        end
+        #(CLK_PERIOD);
+
+        assert (pB_wb_data_o[15:0] == 16'hbeef)
+        else begin
+            $error("pB_wB_data_o Failed: 0x%x != 0x%x", pB_wb_data_o[15:0], 16'hbeef);
+            $finish();
+        end
+
+        #(CLK_PERIOD);
+        assert (pB_wb_stall_o == 1)
+        else begin
+            $error("pB_wB_stall_o Failed: 0x%x != 0x%x", pB_wb_stall_o, 1);
+            $finish();
+        end
+
+        assert (pA_wb_data_o == 32'hfeedfeed)
+        else begin
+            $error("pA_wB_data_o Failed: 0x%x != 0x%x", pA_wb_data_o, 32'hfeedfeed);
+            $finish();
+        end
+
         $finish();
     end
 endmodule
